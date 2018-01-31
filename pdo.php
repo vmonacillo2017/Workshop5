ab15cb1f23bf511e3b4d26ecc5db0d4dfca40d57
vmonacillo2017 <34480196+vmonacillo2017@users.noreply.github.com>
2018-02-01 07:48:34 +0800

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -1,6 +1,3 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=misc', 
-   'fred', 'zap');
-
-
-
+$pdo = new PDO('mysql:host=sql12.freesqldatabase.com;port=3306;dbname='sql12217878', 
+   'sql12217878', 'CaxkAGiCjB');
