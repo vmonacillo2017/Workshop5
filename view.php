b79153b152c18748c6176c953eef312674dd63b5
vmonacillo2017 <34480196+vmonacillo2017@users.noreply.github.com>
2018-02-03 10:57:58 +0800

    Update view.php

--- view.php
+++ view.php
@@ -1,10 +1,13 @@
 <?php
 session_start();
-
-if(isset($_GET['logout'])){
-	session_start();
-	session_destroy();
-	header('location:index.php');
+if(!isset($_GET['name'])){
+	die("Not logged in");
+}else if(isset($_GET['logout'])){
+		session_start();
+		session_destroy();
+		header('location:index.php');
+}else{
+	$name = $_GET['name'];
 }
 ?>
 <!DOCTYPE html>
